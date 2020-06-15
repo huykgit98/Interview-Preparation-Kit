41629b74498c396d3a03295454dd605bd2718636
Negan Nguyen <huykgit98@gmail.com>
2020-06-15 12:25:46 +0700

    upload Counting Valleys

---
+++ Warm-up-Challenges/Counting-Valleys.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+        int result = 0;
+        int current = 0;
+        char[] ch = s.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) 
+        {
+            if(ch[i]=='U')
+            {
+                current++;
+                if(current==0) result++;
+            }
+            if(ch[i]=='D')
+                current--;
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
